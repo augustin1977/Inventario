c767336785e50d7a769c6350b33366693036582a
Eric Augustin <ericaugustin@hotmail.com>
2024-07-06 20:52:27 -0300

    ajustando Banco de dados

--- Materiais/models.py
+++ Materiais/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from djmoney.models.fields import *
 
+
 class Localizacao(models.Model):
     superintendencia=models.CharField(max_length=255,default="SP")
     cidade=models.CharField(max_length=255)
@@ -11,15 +12,22 @@ class Localizacao(models.Model):
 
     def __str__(self):
         return f"{self.superintendencia} - {self.gerencia} - {self.cidade} - {self.endereco}"
+
+class Estado_bem(models.Model):
+    estado=models.CharField(max_length=255,default="Bom")
+    def __str__(self):
+        return f"{self.estado}"
+
 class Material(models.Model):
-    RGP = models.CharField(max_length=255,unique=True)
+    RGP = models.CharField(verbose_name="Numero RGP",max_length=255,unique=True)
     data_cadastro=models.DateTimeField(auto_now_add=True)
     data_atualizacao=models.DateTimeField(auto_now=True)
     nome = models.CharField(max_length=255)
     localizacao=models.ForeignKey(Localizacao, on_delete=models.CASCADE)
-    foto=models.FileField()
-    obs=models.TextField(null=True,blank=True)
-    ativo=models.BooleanField(default=True)
+    foto1=models.ImageField(verbose_name="Foto do Item")
+    estado=models.ForeignKey(Estado_bem,on_delete=models.CASCADE)
+    obs=models.TextField(verbose_name="Observação",null=True,blank=True)
+    ativo=models.BooleanField(verbose_name="Item em uso?",default=True)
     valor=MoneyField(max_digits=19, decimal_places=4, null=True, default_currency="BRL") # type: ignore
 
     def __str__(self):
