45099dd22a835667126a7bb185ab1a7128d64c4e
Eric_Augustin <ericaugustin@hotmail.com>
2024-07-29 16:09:06 -0300

    Corrigindo erro de view.py

--- Materiais/views.py
+++ Materiais/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from .models import *
 from django.core.paginator import Paginator
 from .forms import *
