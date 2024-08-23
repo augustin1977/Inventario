88692807a56036d390703bc32633512dd1554c6f
Eric_Augustin <ericaugustin@hotmail.com>
2024-08-22 23:23:21 -0300

    aumentando a seguranca

--- inventario/settings.py
+++ inventario/settings.py
@@ -20,7 +20,7 @@ import secret
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-20f7wn-1*!9q+%*jg*)vin%^^dgqj(4897#we_@vc5k--yo**m'
+SECRET_KEY = secret.secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
