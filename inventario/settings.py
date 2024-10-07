b8576cc3da0605b4889fa5a2fc902627a220a869
Eric_Augustin <ericaugustin@hotmail.com>
2024-10-07 19:35:35 -0300

    acrescentando novo host

--- inventario/settings.py
+++ inventario/settings.py
@@ -23,7 +23,7 @@ import secret
 SECRET_KEY = secret.secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.62',
                  'localhost',
