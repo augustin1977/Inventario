e1c48704027ce5a594bb05d6305fc9dbab6b1dad
Eric_Augustin <ericaugustin@hotmail.com>
2024-10-11 15:08:34 -0300

    corrigindo arquivos estaticos

--- inventario/settings.py
+++ inventario/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = secret.secretkey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.62',
-                 'localhost',
+ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  'inventariosp.ddns.net',
                  'casaaugustin.zapto.org',
