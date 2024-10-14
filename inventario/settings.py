b8d5e2d6ca9d99150401dbfe9f3926dfcc725524
Eric_Augustin <ericaugustin@hotmail.com>
2024-10-14 18:25:28 -0300

    adicionando novos endereços

--- inventario/settings.py
+++ inventario/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
-                 'inventariomte.zaptp.org',
+                 'inventariomte.zapto.org',
                  'casaaugustin.zapto.org',
                  'inventariosp.zapto.org',
                  '192.168.0.97']
